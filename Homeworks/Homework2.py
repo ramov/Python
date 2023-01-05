3958e6b38bae464c08f1cad4a4010445b7f5b127
Rashid Salimov <salimovrashid@yandex.ru>
2023-01-05 19:43:37 +0400

    Added Seminar3 + edits

--- Homeworks/Homework2.py
+++ Homeworks/Homework2.py
@@ -20,14 +20,25 @@ while i < len(my_list):
     i += 1
 print(sum_num)
 
+# Второй вариант решения:
+
+# number = input('Input number: ')
+# sum_num = 0
+
+# for i in number:
+#   if item.isdigit():
+#       sum_num += int(i)
+# print(sum_num)
+
+
 '''
 
 # ===== Задание 2:
-# Напишите программу, которая принимает на вход число N и
-# выдает набор произведений чисел от 1 до N.
+# Напишите программу, которая принимает на вход число num и
+# выдает набор произведений чисел от 1 до num.
 
 # Пример:
-# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+# - пусть num = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 '''
 num = int(input('Input number: '))
@@ -65,16 +76,17 @@ print(f'n = {num}: {dictionary} \nСумма = {sum(dict.values(dictionary))}')
 '''
 
 # ===== Задание 4:
-# Задайте список из N элементов,
-# заполненных числами из промежутка [-N, N].
+# Задайте список из num элементов,
+# заполненных числами из промежутка [-num, num].
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt
 # в одной строке одно число.
 
+'''
 
 import random
 
-num = int(input('Input N: '))
+num = int(input('Input num: '))
 
 my_lst = []
 for n in range(-num, num + 1):
@@ -92,6 +104,7 @@ print()
 print(f'Product numbers = {product_numbers}')
 f.close()
 
+'''
 
 # ===== Задание 5:
 # Реализуйте алгоритм перемешивания списка.
@@ -102,6 +115,20 @@ import random
 my_lst = [1, 2, 3, 'asd', 'vt', 'nq', 34.45, 1.05]
 print(my_lst)
 random.shuffle(my_lst)
+print(my_lst)
+
+
+Второй вариант решения:
+
+import random
+
+my_lst = [1, 2, 3, 'asd', 'vt', 'nq', 34.45, 1.05]
+print(my_lst)
+
+for el in range(0, len(my_lst)):
+    i = int(random.randint(0, len(my_lst) - 1))
+    my_lst[i], my_lst[el] = my_lst[el], my_lst[i]
+
 print(my_lst)
 
 '''
